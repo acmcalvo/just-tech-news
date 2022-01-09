0b9bc0fe8b221def4749969194973cf4b69b3873
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-09 14:10:51 -0500

    create dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
-//import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-// create connection to our database, pass in your MYSQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+require('dotenv').config();
+
+// create connection to our db, im not sure if i need provided my password here.
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
-module.exports = sequelize
+
+module.exports = sequelize;
